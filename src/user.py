e9ba76520075046f293abb36aff42be9a28692c1
Charles Aina <aina1@ualberta.ca>
2022-03-26 16:04:54 -0600

    q1 and 2 refractored

--- src/user.py
+++ src/user.py
@@ -1,5 +1,6 @@
 from lib2to3.pgen2.token import MINUS
 from tkinter import N
+from unicodedata import category
 from connection import collections
 from pprint import pprint
 
@@ -25,11 +26,11 @@ class User():
 
         if (len(strings) > 0 and len(years) > 0):
             result = self.basics.find(
+                # maybe use all for the regex
                 {'$and': [
                     {'$text': {'$search': regex}},
                     {'startYear': {'$in': years}}
                 ]}
-                #  {'$or':[{'$text':{'$search':regex}},{'startYear':{'$in':years}}]}
             )
         elif len(years) == 0:
             result = self.basics.find(
@@ -44,40 +45,69 @@ class User():
             print("\n")
             all_movies.append(r['primaryTitle'].lower())
 
-        movie = ''
-        while movie.lower() not in all_movies:
-            movie = input("select a Valid Title to get more information on: ")
 
-        tconst = self.basics.find({'primaryTitle':{'$regex':movie,'$options':'i'}})
-        tconst = list(tconst)[0]['tconst']
+        if (input("Would you like to choose a specific Movie? (yes/no): ").lower() == "yes"):
+            movie = ''
+            while movie.lower() not in all_movies:
+                movie = input("select a Valid Title to get more information on: ")
 
-        movie_rating = self.ratings.find(
-            {'tconst': tconst},
-            {'_id': movie, 'Ratings': '$averageRating', 'Number of Votes': '$numVotes'}
-        )
+            tconsts = self.basics.aggregate([
+            {'$match':{'primaryTitle':{'$regex':'^' + movie + '$','$options':'i'}}},
+            {'$lookup':{
+                'from': 'title_ratings',
+                'localField' : 'tconst',
+                'foreignField' : 'tconst',
+                'as' : 'ratings'
+            }},
+            {'$unwind':'$ratings'}, 
+            {'$project':{'_id':0,'primaryTitle':'$primaryTitle','tconst':'$tconst','numVotes':'$ratings.numVotes','rating':'$ratings.averageRating'}} 
+            ])
+            ratings = list(tconsts)[0]
+            tconst = ratings['tconst']
 
-        nconst = self.principals.find({'tconst': tconst})
-        nconst_list = list(nconst)
-        for i in range(0, len(nconst_list)):
-            nconst_list[i] = nconst_list[i]['nconst']
+        
 
-        casts = self.name.find({'nconst': {'$in': nconst_list}})
-        casts_list = list(casts)
-        for i in range(len(casts_list)):
-            casts_list[i] = casts_list[i]['primaryName']
+            nconst = list(self.principals.find({'tconst': tconst}))
+            nconst_list = list(nconst)
+            for i in range(0, len(nconst_list)):
+                nconst_list[i] = nconst_list[i]['nconst']
+              
+            
+            for i in tconsts:
+                print(i)
 
-        for r in movie_rating:
-            pprint(r)
-        print("These are the Cast Members of this movie:")
-        for i in casts_list:
-            print(i)
+
+            print("\nHere is info about the Movie")
+            print("Title: " + ratings['primaryTitle'] + ' | Number of Votes:  ' + str(ratings['numVotes']) + ' | Rating:  ' + str(ratings['rating']))
+
+            names = list(self.name.find(
+                {'nconst':{'$in':nconst_list}},
+                {'nconst':'$nconst','primaryName':'$primaryName'}
+            ))
+
+            print("\nHere are the names of the Cast, their Category and the name of the Character they played\n")
+            for cast in names:
+                characters_str = ""
+                for n_const in nconst:
+                    if cast['nconst'] == n_const['nconst']:
+                        if n_const['characters'] != None:
+                            length = len(n_const['characters'][0])
+                            characters_str += n_const['characters'][0][1:length-1]
+                        else:
+                            characters_str = "None"
+                        print(cast['primaryName'] + ' | ' + n_const['category'] + ' | ' + characters_str)
+
+            
+
+
+           
 
 
     def search_for_genres(self,genres,minimum_vote):
         minimum_vote = int(minimum_vote)
         result = self.basics.aggregate([
                 {'$unwind':'$genres'},
-                {'$match':{'genres':{'$regex':genres,'$options':'i'}}},
+                {'$match':{'genres':{'$regex':'^'+ genres + '$','$options':'i'}}},
                 {'$group':{'_id':'$tconst'}},
                 {'$project':{'tconst':'$_id',"_id":0}}
             ]
@@ -87,34 +117,37 @@ class User():
         for r in result:
             result_list.append(r['tconst'])
 
+
         new_result = self.ratings.aggregate([
             {'$match':{'numVotes':{'$gte':minimum_vote}}},
             {'$match':{'tconst':{'$in':result_list}}},
             {'$sort':{'averageRating':-1}},
-            {'$project':{'tconst':'$tconst',"_id":0}}
+            {'$project':{'tconst':'$tconst','Average':'$averageRating','Votes':'$numVotes',"_id":0}}
         ]
         )
 
+        new_result_list = list(new_result)
         filtered_list = []
-        for r in new_result:
+        for r in new_result_list:
             filtered_list.append(r['tconst'])
 
-
-        last_result = self.basics.aggregate([
+        last_result = list(self.basics.aggregate([
             {'$match':{'tconst':{'$in':filtered_list}}},
-            {'$project':{'Title of Movie':'$primaryTitle','_id':0}}
-        ])
+            {'$project':{'tconst':'$tconst','Title of Movie':'$primaryTitle','_id':0}}
+        ]))
+        for j in range(0,len(new_result_list)):        
+            for i in range(0,len(last_result)):
+                if last_result[i]['tconst'] == new_result_list[j]['tconst']:
+                    print(last_result[i]['Title of Movie'], '|' , str(new_result_list[j]['Average']) ,'|' , str(new_result_list[j]['Votes']))
+                    break
 
-
-        for r in last_result:
-            print(r)
-    
+        
         
 
 
 user = User()
 keywords = input("what are your keywords: ").split()
-user.search_for_titles(keywords)
-# user.search_for_genres(keywords[0],keywords[1])
+# user.search_for_titles(keywords)
+user.search_for_genres(keywords[0],keywords[1])
 
 
