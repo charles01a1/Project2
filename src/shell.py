c1695c1c94714fc1e31c3d0dcdb2ebd1d7706233
Dekr0 <dekr0.dk@gmail.com>
2022-03-26 22:52:12 -0600

    update shell.py
    1. print_help (WIP)
    2. bug fix

--- src/shell.py
+++ src/shell.py
@@ -11,31 +11,37 @@ class Shell:
             "-m": self.usr.add_movie,
         }
 
-        if add_option in add_options_dict:
+        if add_option in add_options_dict.keys():
             args = args_str.split("|")
 
             add_options_dict[add_option](*args)
 
     def search(self, args_str, search_option):
         search_options_dict = {
-            "-c": self.usr.search_for_members,
+            "-m": self.usr.search_for_members,
             "-g": self.usr.search_for_genres,
             "-t": self.usr.search_for_titles,
         }
 
-        if search_option in search_options_dict:
+        if search_option in search_options_dict.keys():
             args = args_str.split("|")
 
             search_options_dict[search_option](*args)
 
     def print_help(self):
-        pass
+        self.add_help()
+        self.search_help()
 
     def add_help(self):
-        pass
+        print("add usage:\n"
+              "add|[-c]|[member_id]|[title_id]|[category]\n"
+              "add|[-m]|[title_id]|[title]|[start_year]|[runtime]|[list_of_genres]\n")
 
     def search_help(self):
-        pass
+        print("search usage:\n"
+              "search|[-g]|[genre]|[minimum_vote_count]\n"
+              "search|[-t]|[keyword_1]|[keyword_2]|...|[keyword_n]\n"
+              "search|[")
 
     def main_menu(self):
         cmds_dict = {
@@ -51,10 +57,10 @@ class Shell:
                 line = input("$ ").strip()
 
                 if line:
-                    cmd_name, rest = line.split(" ", 1)
+                    cmd_name, rest = line.split("|", 1)
                     if cmd_name in cmds_dict:
-                        option, args_str = line.split(" ", 1)
-                        cmds_dict[cmd_name](option, args_str)
+                        option, args_str = rest.split("|", 1)
+                        cmds_dict[cmd_name](args_str, option)
                     elif cmd_name == "exit" or cmd_name == "quit":
                         return
                     else:
